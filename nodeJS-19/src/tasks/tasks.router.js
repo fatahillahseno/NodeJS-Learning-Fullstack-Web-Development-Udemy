bf51fc5990bc99f255efcbc883ba14b77fececfe
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-11-05 09:59:23 +0700

    membuat dokumentasi untuk get tasks api

--- nodeJS-19/src/tasks/tasks.router.js
+++ nodeJS-19/src/tasks/tasks.router.js
@@ -9,6 +9,80 @@ const deleteTaskValidator = require("./validators/deleteTask.validator.js");
 const tasksRouter = express.Router();
 const authenticateToken = require("../middleware/authenticateToken.middleware.js");
 
+/**
+ * @swagger
+ *
+ * components :
+ *  securitySchemes:
+ *    bearerAuth:
+ *      type: http
+ *      scheme: bearer
+ *      bearerFormat: JWT
+ *
+ * /tasks:
+ *  get:
+ *    summary: Get all tasks
+ *    tags: [Tasks]
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - in: query
+ *        name: limit
+ *        schema:
+ *          type: integer
+ *          default: 10
+ *        description: The number of tasks needed in a single response
+ *      - in: query
+ *        name: page
+ *        schema:
+ *          type: integer
+ *          default: 1
+ *        description: The page number of the tasks response
+ *      - in: query
+ *        name: order
+ *        schema:
+ *          type: string
+ *          default: 'asc'
+ *          enum: ['asc','dsc']
+ *        description: Order of the tasks
+ *
+ *    responses:
+ *      200:
+ *        description: Task created successfully
+ *        content:
+ *           application/json:
+ *             example:
+ *               status: success
+ *               statusCode: 200
+ *               message: OK
+ *               data:
+ *                - _id: 690820fc610b30435b03465e
+ *                  title: Create a new video
+ *                  description: A video about fullstack web development
+ *                  status: normal
+ *                  priority: high
+ *                  dueDate: 2025-01-01T12:00:00Z
+ *      401:
+ *        description: Not Authorized Error
+ *        content:
+ *           application/json:
+ *             example:
+ *               status: error
+ *               statusCode: 401
+ *               message: Unauthorized
+ *               error:
+ *                 message: You are not authorized to perform this request.
+ *      403:
+ *        description: Forbidden Error
+ *        content:
+ *           application/json:
+ *             example:
+ *               status: error
+ *               statusCode: 403
+ *               message: Forbidden
+ *               error:
+ *                 message: Please login again, invalid token.
+ */
 tasksRouter.get("/tasks", [getTaskValidator, authenticateToken], (req, res) => {
   const result = validationResult(req);
 
@@ -28,6 +102,7 @@ tasksRouter.get("/tasks", [getTaskValidator, authenticateToken], (req, res) => {
  *      type: http
  *      scheme: bearer
  *      bearerFormat: JWT
+ *
  * /tasks:
  *  post:
  *    summary: Create a new task
